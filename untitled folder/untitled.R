434350cbf3b97d39dbd5267fc932750ca6434179
Patrick Witczak <patrick.witczak@web.de>
2025-09-01 11:01:33 +0800

    Created 2 tibbles with my primary and some secondary variables for the years 2019 and 2022

---
+++ untitled folder/untitled.R
@@ -0,0 +1,91 @@
+
+# All needed Libraries
+library(tidyverse)
+
+# Source: Statistisches Bundesamt (Destatis, 2021)
+# Ausgwählte Kennzahlen nach Fachabteilung 2019
+KHR_2019 <- tribble(
+  ~Abteilung,  ~Abt_Anzahl, ~Abt_Betten, ~Nutzungsgrad, ~Fallzahl, ~Avg_Verweildauer,
+  "Innere Medizin", 1047, 111481, 78.1, 5889078, 5.4,
+  "Geriatrie", 322, 18101, 86.9, 374462, 15.3,
+  "Kardiologie", 206, 14800, 83.6, 955424, 4.7,
+  "Nephrologie", 62, 2186, 83.5, 96053, 6.9,
+  "Hämatologie und internistische Onkologie", 102, 5096, 79.9, 201198, 7.4,
+  "Endokrinologie", 18, 501, 83.3, 23432, 6.5,
+  "Gastroenterologie", 126, 7203, 82.5, 418259, 5.2,
+  "Pneumologie", 64, 3689, 79.4, 178007, 6.0,
+  "Rheumatologie", 33, 1096, 72.1, 38126, 7.6,
+  "Pädiatrie", 339, 15447, 63.2, 926516, 3.8,
+  "Kinderkardiologie", 23, 588, 65.0, 19087, 7.3,
+  "Neonatologie", 99, 2097, 72.3, 56492, 9.8,
+  "Kinderchirugie", 83, 1592, 63.8, 124763, 3.0,
+  "Lungen- und Bronchialheilkunde", 18, 1775, 69.5, 62763, 7.2,
+  "Allgemeine Chirugie", 1068, 67902, 69.8, 3195674, 5.4,
+  "Unfallchirugie", 308, 17303, 78.1, 840038, 5.9,
+  "Neurochirugie", 178, 6642, 77.6, 256956, 7.3,
+  "Gefäßchirugie", 186, 5529, 72.3, 189430, 7.7,
+  "Plastische Chirugie", 132, 1936, 69.2, 89349, 5.5,
+  "Thoraxchirugie", 56, 1546, 67.8, 50570, 7.6,
+  "Herzchirugie", 71, 4614, 77.5, 152524, 8.6,
+  "Urologie", 500, 14036, 74.4, 886904, 4.3,
+  "Orthopädie", 435, 23078, 68.5, 904191, 6.4,
+  "Frauenheilkunde und Geburtshilfe", 759, 25039, 60.8, 1548418, 3.6,
+  "Geburtshilfe", 86, 2205, 78.3, 180552, 3.5,
+  "Hals-, Nasen-, Ohrenheilkunde", 592, 8672, 60.5, 562945, 3.4,
+  "Augenheilkunde", 264, 4219, 65.4, 360377, 2.8,
+  "Neurologie", 455, 26716, 81.2, 1092503, 7.2,
+  "Allgemeine Psychiatrie", 394, 57269, 94.0, 769076, 24.7,
+  "Kinder- und Jugendpsychiatrie", 144, 6696, 88.7, 59578, 36.4,
+  "Psychosomatik/Psychotherapie", 275, 12394, 90.1, 93160, 43.7,
+  "Nuklearmedizin", 96, 724, 44.9, 36061, 3.3,
+  "Strahlenheilkunde", 145, 2489, 68.8, 70938, 8.8,
+  "Dermatologie", 109, 4575, 77.3, 240506, 5.4,
+  "Zahn- und Kieferheilkunde, Mund- und Kieferchirugie", 183, 2041, 65.4, 116377, 4.2,
+  "Intensivmedizin", 260, 6568, 77.0, 474544, 3.9,
+  "Sonstige Fachabteilung", 305, 6481, 73.5, 269292, 6.5
+)
+
+# Source: Statistisches Bundesamt (Destatis, 2024)
+# Ausgwählte Kennzahlen nach Fachabteilung 2022
+KHR_2022 <- tribble(
+  ~Abteilung,  ~Abt_Anzahl, ~Abt_Betten, ~Nutzungsgrad, ~Fallzahl, ~Avg_Verweildauer,
+  "Innere Medizin", 1002, 102760, 69.6, 4881453, 5.3,
+  "Geriatrie", 337, 18380, 71.6, 315071, 15.3,
+  "Kardiologie", 260, 18316, 72.0, 1029244, 4.7,
+  "Nephrologie", 67, 2230, 75.7, 88989, 6.9,
+  "Hämatologie und internistische Onkologie", 121, 5452, 74.9, 200535, 7.4,
+  "Endokrinologie", 14, 498, 66.7, 16031, 7.6,
+  "Gastroenterologie", 155, 8137, 77.3, 423211, 5.4,
+  "Pneumologie", 85, 4495, 70.3, 196123, 5.9,
+  "Rheumatologie", 34, 1170, 59.0, 33312, 7.6,
+  "Pädiatrie", 326, 14920, 57.8, 830083, 3.8,
+  "Kinderkardiologie", 22, 506, 61.3, 17209, 6.6,
+  "Neonatologie", 103, 2143, 63.5, 50565, 9.8,
+  "Kinderchirugie", 82, 1502, 56.4, 112573, 2.7,
+  "Lungen- und Bronchialheilkunde", 17, 1381, 59.9, 43949, 6.9,
+  "Allgemeine Chirugie", 1061, 63342, 61.2, 2730873, 5.2,
+  "Unfallchirugie", 354, 18255, 69.8, 825194, 5.6,
+  "Neurochirugie", 186, 6554, 69.8, 242866, 6.9,
+  "Gefäßchirugie", 205, 5581, 64.1, 176349, 7.4,
+  "Plastische Chirugie", 142, 2029, 57.9, 87850, 4.9,
+  "Thoraxchirugie", 69, 1623, 60.7, 52361, 6.9,
+  "Herzchirugie", 71, 4291, 66.9, 124049, 8.4,
+  "Urologie", 492, 13431, 65.4, 807178, 4.0,
+  "Orthopädie", 377, 19381, 60.2, 712411, 6.0,
+  "Frauenheilkunde und Geburtshilfe", 722, 23119, 54.8, 1394024, 3.3,
+  "Geburtshilfe", 74, 2235, 64.7, 162564, 3.2,
+  "Hals-, Nasen-, Ohrenheilkunde", 539, 7583, 51.0, 437442, 3.2,
+  "Augenheilkunde", 236, 4000, 56.3, 308103, 2.7,
+  "Neurologie", 470, 26948, 73.0, 985359, 7.3,
+  "Allgemeine Psychiatrie", 403, 57011, 86.3, 746209, 24.1,
+  "Kinder- und Jugendpsychiatrie", 144, 6763, 82.7, 64032, 31.9,
+  "Psychosomatik/Psychotherapie", 279, 12844, 84.0, 89699, 43.9,
+  "Nuklearmedizin", 92, 748, 42.1, 34508, 3.3,
+  "Strahlenheilkunde", 141, 2250, 59.0, 57839, 8.4,
+  "Dermatologie", 111, 4379, 63.2, 197743, 5.1,
+  "Zahn- und Kieferheilkunde, Mund- und Kieferchirugie", 169, 1890, 56.5, 98140, 4.0,
+  "Intensivmedizin", 286, 7558, 67.4, 467038, 4.0,
+  "Sonstige Fachabteilung", 306, 6677, 65.3, 283321, 5.6
+)
+
+
